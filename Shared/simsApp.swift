6747f76cd31477ee7387bb6eda95c418454f3537
Will-Carrara <wicarrara@csumb.edu>
2020-12-20 11:06:08 -0800

    Initial Commit

---
+++ Shared/simsApp.swift
@@ -0,0 +1,17 @@
+//
+//  simsApp.swift
+//  Shared
+//
+//  Created by Will Carrara on 12/20/20.
+//
+
+import SwiftUI
+
+@main
+struct simsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
